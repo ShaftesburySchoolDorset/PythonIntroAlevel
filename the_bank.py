d6ce92b3a1be6e9f991c027e02f2d5ef5032ee7b
George Complin <gcomplin@gmail.com>
2016-11-08 10:51:44 +0000

    added the_bank.py

---
+++ the_bank.py
@@ -0,0 +1,45 @@
+class BankAccount(object):
+	
+	def __init__(self, person_name, opening_balance = 0):
+		self.balance = opening_balance
+		self.name = person_name
+		print("New account created for: {}".format(self.name))
+
+	@staticmethod
+	def bank_name():
+		return "Nationwide"
+
+	# Withdrawn method
+	def withdraw(self, amount):
+		if amount > self.balance:
+			print("Sorry, insufficient funds available. You have: £{}".format(self.balance))
+		else:
+			self.balance -= amount
+			print("£{} has been withdrawn. New balance: £{}".format(amount, self.balance))
+			
+	def deposit(self, amount):
+		self.balance += amount
+		print("£{} has been deposited. New balance: £{}".format(amount, self.balance))
+		
+	def transaction(self, amount, withdraw, other_account):
+		if withdraw:
+			if amount > self.balance:
+				print("Insufficient funds")
+			else:
+				self.withdraw(amount)
+				other_account.deposit(10)
+		else:
+			other_account.withdraw(10)
+			self.deposit(10)
+
+
+# code
+a = BankAccount("Joe Bloggs", 50)
+b = BankAccount("Jacob Smirthwiat", 47)
+# account a gives £10 to account b
+a.transaction(10, True, b)
+
+
+
+	
+	
