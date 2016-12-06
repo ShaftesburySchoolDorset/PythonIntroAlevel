33f5c03b74eb07cc430fc9808e0fbd7b4797d517
George Complin <gcomplin@gmail.com>
2016-12-06 10:56:54 +0000

    updated exceptions

--- Exceptions/exceps.py
+++ Exceptions/exceps.py
@@ -1,20 +1,24 @@
+# Run this code first
 try:
 	fh = open('stuff.txt')
 	print(fh.read())
 	
 	a = 1
 	b = '3'
-	#c = a + b
+	#c = a + b # will error
 	
+# Catch an IO Error
 except IOError:
 	print('Error! Can\'t find file')
 
+# Catch all other errors
 except Exception as e:
 	print(e)
-	
+
+# Do something if the code in 'try' worked
 else:
 	print('It worked!')
-	
+
+# Always end with this code
 finally:
-	
 	print('we got to the end!')
